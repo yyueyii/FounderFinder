435ea809da2694d1a53dfb0cb3c092deb96214f3
yyueyii <156628825+yyueyii@users.noreply.github.com>
2024-06-02 22:09:35 +0800

    Update successful-match.jsx

--- app/successful-match.jsx
+++ app/successful-match.jsx
@@ -15,14 +15,20 @@ const MatchedPopUp = ({ name, image, visible, onLater, onMessage }) => {
     >
       <View style={styles.modalBackground}>
         <View style={styles.modalContainer}>
+
           <Text style={styles.messageText}>You matched with</Text>
+
           <Text style={[styles.messageText, {fontSize: 24}]}>Annabelle Farber</Text>
-          <View style={{width:styles.modalContainer.height*0.5, height:styles.modalContainer.height*0.5, backgroundColor:'white', margin:10, marginBotton:30}}></View>
-          <TouchableOpacity onPress={onLater} style={styles.closeButton}>
-            <Text style={styles.buttonText}>Later</Text>
+
+          {/* temp image */}
+          <View style={{width:styles.modalContainer.height*0.5, height:styles.modalContainer.height*0.5, backgroundColor:'gray', margin:10, marginBotton:30}}></View>
+          
+          <TouchableOpacity onPress={onLater} style={styles.laterButton}>
+            <Text style={[styles.buttonText, {color:'#4A0AFF'}]}>Later</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={onMessage} style={styles.closeButton}>
-            <Text style={styles.buttonText}>Message</Text>
+          
+          <TouchableOpacity onPress={onMessage} style={styles.messageButton}>
+            <Text style={[styles.buttonText, {color:'white'}]}>Message</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -35,7 +41,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(255, 100, 255, 0.5)', 
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', 
   },
   modalContainer: {
     width: width*0.7,
@@ -51,10 +57,29 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     marginBottom: 20,
   },
-  closeButton: {
-    backgroundColor: 'blue',
-    padding: 10,
+  laterButton: {
+    backgroundColor: 'white',
+    paddingLeft:30,
+    paddingRight:30,
+    height:35,
     borderRadius: 5,
+    borderColor:'#4A0AFF',
+    borderWidth:2,
+    alignItems:'center',
+    justifyContent:'center',
+    top:15,
+    left:80,
+  },
+  messageButton: {
+    backgroundColor:'#4A0AFF',
+    paddingLeft: 30,
+    paddingRight: 30,
+    height:35,
+    borderRadius: 5,
+    alignItems:'center',
+    justifyContent:'center',
+    top:-20,
+    left:-80,
   },
   buttonText: {
     color: 'black',
